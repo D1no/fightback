e95c174aaaa554ad1050c284f3ee323ea9e1b4ab
Dino Scheidt <github@dino-scheidt.de>
2019-04-28 23:29:58 +0200

    ✨💩 Add preloader to make first load more friendly. Needs proper promise from firebase.

--- src/App.js
+++ src/App.js
@@ -14,6 +14,25 @@ class App extends Component {
       </Provider>
     );
   }
+
+  // ToDo This needs to come from suspense. Using this: https://codesandbox.io/s/6l65nq9k3n
+  loadingpromise() {
+    return new Promise(resolve => setTimeout(resolve, 1500));
+  }
+
+  componentDidMount() {
+    this.loadingpromise().then(() => {
+      const ele = document.getElementById("ipl-progress-indicator");
+      if (ele) {
+        // fade out
+        ele.classList.add("available");
+        setTimeout(() => {
+          // remove from DOM
+          ele.outerHTML = "";
+        }, 2000);
+      }
+    });
+  }
 }
 
 export default App;
